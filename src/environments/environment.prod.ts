fd9b6a5a5af01ef06e5238978151c62da79fd7be
simran442 <smmkaur4@gmail.com>
2021-06-14 16:27:41 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://142.93.217.129:3005/',
-  loginBaseUrl: 'https://142.93.217.129:3005/',
-  frontendBaseUrl: 'https://localhost:4200/'
+  baseUrl: 'http://142.93.217.129:3005/',
+  loginBaseUrl: 'http://142.93.217.129:3005/',
+  frontendBaseUrl: 'http://localhost:4200/'
 };
