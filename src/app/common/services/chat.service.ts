26999b14ade3285d29e15531332b6620acfa8921
simran442 <smmkaur4@gmail.com>
2021-08-09 22:11:18 +0530

    bug fixed

--- src/app/common/services/chat.service.ts
+++ src/app/common/services/chat.service.ts
@@ -8,24 +8,28 @@ export class ChatService {
   socket;
   constructor() {   }
 
-  setupSocketConnection() {
-    this.socket = io('http://localhost:3000', {
+  setupSocketConnection(message) {
+    this.socket = io('http://localhost:3025', {
       auth: {
         token: "abc"
       }
     });
+    // this.socket.emit('my message', message);
 
-    this.socket.emit('my message', 'Hello there from Angular.');
-
-    this.socket.on('my broadcast', (data: string) => {
-      console.log(data);
-    });
+    // this.socket.on('my broadcast', (data: string) => {
+    //   console.log(data);
+    // });
   }
   
   disconnect() {
     if (this.socket) {
       this.socket.disconnect();
     }
+  }
+
+
+  sendMessage(message){
+    this.socket.emit('message', message);
   }
 }
 
--- src/app/modules/messages/messages.component.ts
+++ src/app/modules/messages/messages.component.ts
@@ -9,21 +9,52 @@ import { ChatService } from '../../../app/common/services/chat.service';
 export class MessagesComponent implements OnInit {
   socket;
   newMessage;
+  message
   messageList: string[] = [];
   constructor(private chatService: ChatService) {
   }
 
   ngOnInit() {
-    this.chatService.setupSocketConnection();
+    this.setupSocketConnection()
+   // this.chatService.setupSocketConnection(this.newMessage);
   }
+
+  setupSocketConnection() {
+    this.socket = io('http://localhost:3025');
+    this.socket.on('message-broadcast', (data: string) => {
+      if (data) {
+       const element = document.createElement('p');
+       element.innerHTML = data;
+       element.style.background = 'white';
+       element.style.padding =  '15px 30px';
+       element.style.margin = '10px';
+       document.getElementById('message-lists').appendChild(element);
+       }
+     });
+ }
 
   ngOnDestroy() {
     this.chatService.disconnect();
   }
 
   sendMessage() {
-    //this.chatService.sendMessage(this.newMessage);
-    this.newMessage = '';
+    this.chatService.sendMessage(this.newMessage);
+    //this.newMessage = '';
   }
+
+  SendMessage() {
+    this.socket.emit('message', this.message);
+    const element = document.createElement('p');
+    element.innerHTML = this.message;
+    element.style.background = 'white';
+    element.style.padding =  '15px 30px';
+    element.style.margin = '10px';
+    element.style.textAlign = 'right';
+    document.getElementById('message-lists').appendChild(element);
+    this.message = '';
+ }
+
+
+
 
 }
